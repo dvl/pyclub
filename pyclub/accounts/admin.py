48957552ef5fea259ee2f1855cba6e4c8ef152a2
André Luiz <contato@xdvl.info>
2015-04-10 22:54:19 -0300

    Aponta o form do admin para model personalizada

--- pyclub/accounts/admin.py
+++ pyclub/accounts/admin.py
@@ -4,4 +4,9 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(get_user_model(), UserAdmin)
+from . import forms
+
+
+@admin.register(get_user_model())
+class UsuarioAdmin(UserAdmin):
+    add_form = forms.UserCreationForm
