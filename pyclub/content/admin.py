d5821404d15f23405f65aebe699c6709ba1d0b05
André Luiz <contato@xdvl.info>
2015-04-15 11:30:35 -0300

    Exibe os posts no admin

--- pyclub/content/admin.py
+++ pyclub/content/admin.py
@@ -5,13 +5,21 @@ from django.contrib import admin
 from . import models
 
 
-class RevisionInline(admin.TabularInline):
+class RevisionInline(admin.StackedInline):
     model = models.Revision
     extra = 0
 
 
 @admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'revision',
+        'status',
+        'created_at',
+        'created_by',
+    )
+
     inlines = (
         RevisionInline,
     )
