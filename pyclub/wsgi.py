9630cdaf3b13d1d484a4f2ef9ca3cccadaf43dc3
André Luiz <contato@xdvl.info>
2015-07-02 15:02:57 -0300

    Adiciona whitenoise e waitress-server para um possivel deploy no Heroku

--- pyclub/wsgi.py
+++ pyclub/wsgi.py
@@ -3,7 +3,9 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyclub.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
