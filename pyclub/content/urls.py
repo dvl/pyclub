77d3a2b548b25df69569789316ec620d042efee4
André Luiz <contato@xdvl.info>
2015-07-02 15:03:38 -0300

    Adiciona novas views para visualização e edição de posts

--- pyclub/content/urls.py
+++ pyclub/content/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     # url(r'^$', views.PostListView.as_view(), name='list'),
     url(r'^create/$', views.PostCreateView.as_view(), name='create'),
+    url(r'^update/(?P<pk>\d+)/$', views.PostUpdateView.as_view(), name='update'),
 ]
--- pyclub/content/views.py
+++ pyclub/content/views.py
@@ -1,25 +1,34 @@
 # -*- coding: utf-8 -*-
 
+from django.db.models import Q
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.views import generic
+
 from braces import views as braces
 
-from . import forms, models
+from .forms import PostForm
+from .models import Post
 
 
 class PostListView(generic.ListView):
-    model = models.Post
-    queryset = models.Post.objects.live()
+    model = Post
+    queryset = Post.objects.live()
 
 
 class PostDetailView(generic.DetailView):
-    model = models.Post
-    queryset = models.Post.objects.live()
+    model = Post
+
+    def get_queryset(self):
+        return self.model.objects.filter(
+            (Q(status=Post.FINISHED) & Q(approved=True)) |
+            Q(created_by=self.request.user)
+        )
 
 
 class PostCreateView(braces.LoginRequiredMixin, generic.CreateView):
-    model = models.Post
-    form_class = forms.PostForm
+    model = Post
+    form_class = PostForm
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
@@ -30,4 +39,16 @@ class PostCreateView(braces.LoginRequiredMixin, generic.CreateView):
 
 
 class PostUpdateView(braces.LoginRequiredMixin, generic.UpdateView):
-    pass
+    model = Post
+    form_class = PostForm
+
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get(self.pk_url_kwarg, None)
+
+        if not queryset:
+            queryset = self.get_queryset()
+
+        if not self.request.user.is_superuser:
+            queryset = queryset.filter(created_by=self.request.user)
+
+        return get_object_or_404(queryset, pk=pk)
